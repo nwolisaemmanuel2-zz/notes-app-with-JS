f760630afb50ffa8100ec599ab4a3aea28323a48
Nwolisa Emmanuel Ekene <47080616+nwolisaemmanuel2@users.noreply.github.com>
2020-02-09 08:52:34 +0000

    Update notes.js

--- notes.js
+++ notes.js
@@ -4,7 +4,6 @@ const chalk = require('chalk')
 const addNote = (title, body) => {
     const notes = loadNotes()
     const duplicateNote = notes.find((note) => note.title === title)
-
     if (!duplicateNote) {
         notes.push({
             title: title,
