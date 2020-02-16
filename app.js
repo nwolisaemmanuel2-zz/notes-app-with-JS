f65b937f0205c7bbf564a06a73551e74ba6fa0c3
Nwolisa Emmanuel Ekene <47080616+nwolisaemmanuel2@users.noreply.github.com>
2020-02-16 15:35:42 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const chalk = require('chalk')
 const yargs = require('yargs')
 const notes = require('./notes.js')
 
-// Customize yargs version
+// Customize yargs versions
 yargs.version('1.1.0')
 
 // Create add command
